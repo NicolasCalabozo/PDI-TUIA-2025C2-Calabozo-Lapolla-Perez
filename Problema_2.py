4c07b82d1e482090709bc2e6c5d89e7770a82c8d
martinfl95 <91093784+martinfl95@users.noreply.github.com>
2025-10-15 12:55:10 -0300

    Merge main
    Signed-off-by: martinfl95 <91093784+martinfl95@users.noreply.github.com>

--- Problema_2.py
+++ Problema_2.py
@@ -13,22 +13,93 @@ def detectar_lineas(nombre_archivo, umbral_mascara, umbral_vert, umbral_hor):
     coord_y_lineas = list(np.where(proyeccion_horizontal > umbral_hor)[0])
     x_unicas = coord_x_lineas[1::2]
     y_unicas = coord_y_lineas[1::2]
-    print(x_unicas)
-    print(y_unicas)
     #return lineas verticales, lineas horizontales
-    return img, x_unicas, y_unicas
+    return img, mascara_umbral, x_unicas, y_unicas
+
+
+def encontrar_segmentos_verticales(mascara_umbral, lineas_x):
+    """
+    Itera sobre cada coordenada X de una línea y encuentra los
+    segmentos de línea (y_inicio, y_final) en esa columna.
+    """
+    segmentos_por_columna = {}
+    for x in lineas_x:
+        columna_actual = mascara_umbral[:, x]
+        segmentos_en_columna = []
+        en_segmento = False
+        y_inicio = 0
+
+        # Iteramos sobre los píxeles de la columna
+        for y, pixel_es_negro in enumerate(columna_actual):
+            # Transición de blanco a negro -> Inicia un segmento
+            if pixel_es_negro and not en_segmento:
+                en_segmento = True
+                y_inicio = y
+            elif not pixel_es_negro and en_segmento:
+                en_segmento = False
+                y_final = y - 1
+                segmentos_en_columna.append((y_inicio, y_final))
+        if en_segmento:
+            segmentos_en_columna.append((y_inicio, len(columna_actual) - 1))
+            
+        segmentos_por_columna[x] = segmentos_en_columna
+        
+    return segmentos_por_columna
+
+def encontrar_segmentos_horizontales(mascara_umbral, lineas_y):
+    """
+    Itera sobre cada coordenada Y de una línea y encuentra los
+    segmentos de línea (x_inicio, x_final) en esa fila.
+    """
+    segmentos_por_fila = {}
+    for y in lineas_y:
+        fila_actual = mascara_umbral[y, :]
+        segmentos_en_fila = []
+        en_segmento = False
+        x_inicio = 0
+
+        # Iteramos sobre los píxeles de la fila
+        for x, pixel_es_negro in enumerate(fila_actual):
+            # Transición de blanco a negro -> Inicia un segmento
+            if pixel_es_negro and not en_segmento:
+                en_segmento = True
+                x_inicio = x
+            
+            # Transición de negro a blanco -> Termina un segmento
+            elif not pixel_es_negro and en_segmento:
+                en_segmento = False
+                x_final = x - 1
+                segmentos_en_fila.append((x_inicio, x_final))
+
+        # Caso especial: si la línea llega hasta el borde derecho de la imagen
+        if en_segmento:
+            segmentos_en_fila.append((x_inicio, len(fila_actual) - 1))
+            
+        segmentos_por_fila[y] = segmentos_en_fila
+        
+    return segmentos_por_fila
 
 if __name__ == '__main__':
-    img, vert, hor = detectar_lineas('formulario_vacio.png', 180, 170, 200)
-    img_para_dibujar = cv2.cvtColor(img, cv2.COLOR_GRAY2BGR) #type: ignore
+    img, mascara, vert, hor = detectar_lineas('formulario_vacio.png', 180, 170, 200)
+    img_dummy = cv2.cvtColor(img, cv2.COLOR_GRAY2BGR)
+    segmentos_ver = encontrar_segmentos_verticales(mascara,vert)
+    segmentos_hor = encontrar_segmentos_horizontales(mascara,hor)
+    color_horizontal = (255, 0, 0)  # Azul
+    grosor_linea = 2
+    for y, segs in segmentos_hor.items():
+        for x_start, x_end in segs:
+            cv2.line(img_dummy, (x_start, y), (x_end, y), color_horizontal, grosor_linea)
 
-    # más ancha donde antes había dos.
-    cajas_fusionadas = []
-    for j in range(len(hor) - 1):
-        for i in range(len(vert) - 1):
-            y1, y2 = hor[j], hor[j+1]
-            x1, x2 = vert[i], vert[i+1]
-            caja_actual = {'top_left': (x1, y1), 'bottom_right': (x2, y2)}
-            cajas_fusionadas.append(caja_actual)
-    print(cajas_fusionadas)
-    
+    # 3. Dibujar los segmentos verticales (en rojo)
+    color_vertical = (0, 0, 255)  # Rojo
+    for x, segs in segmentos_ver.items():
+        for y_start, y_end in segs:
+            cv2.line(img_dummy, (x, y_start), (x, y_end), color_vertical, grosor_linea)
+            
+    # 4. Mostrar el resultado final
+    plt.figure(figsize=(12, 12))
+    # Convertimos de BGR (OpenCV) a RGB (Matplotlib) para mostrar colores correctos
+    plt.imshow(cv2.cvtColor(img_dummy, cv2.COLOR_BGR2RGB))
+    plt.title('Todos los Segmentos Detectados sobre la Imagen Original')
+    plt.axis('off')
+    plt.show()
