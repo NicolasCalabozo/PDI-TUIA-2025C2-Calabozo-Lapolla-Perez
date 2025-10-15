2e9fae781cccbbf82338b911fac586440116a367
martinfl95 <91093784+martinfl95@users.noreply.github.com>
2025-10-15 13:20:17 -0300

    Correción de distancia mímima
    Signed-off-by: martinfl95 <91093784+martinfl95@users.noreply.github.com>

--- Problema_2.py
+++ Problema_2.py
@@ -17,67 +17,49 @@ def detectar_lineas(nombre_archivo, umbral_mascara, umbral_vert, umbral_hor):
     return img, mascara_umbral, x_unicas, y_unicas
 
 
-def encontrar_segmentos_verticales(mascara_umbral, lineas_x):
+def encontrar_segmentos(mascara_umbral, coordenadas, eje='horizontal', min_largo=5):
     """
-    Itera sobre cada coordenada X de una línea y encuentra los
-    segmentos de línea (y_inicio, y_final) en esa columna.
+    Encuentra segmentos de línea a lo largo de un eje específico,
+    filtrando aquellos que son más cortos que min_largo.
     """
-    segmentos_por_columna = {}
-    for x in lineas_x:
-        columna_actual = mascara_umbral[:, x]
-        segmentos_en_columna = []
+    segmentos_dict = {}
+    
+    for coord in coordenadas:
+        if eje == 'horizontal':
+            linea_actual = mascara_umbral[coord, :]
+        elif eje == 'vertical':
+            linea_actual = mascara_umbral[:, coord]
+        segmentos_en_linea = []
         en_segmento = False
-        y_inicio = 0
-        for y, pixel_es_negro in enumerate(columna_actual):
+        inicio = 0
+
+        for i, pixel_es_negro in enumerate(linea_actual):
             if pixel_es_negro and not en_segmento:
                 en_segmento = True
-                y_inicio = y
+                inicio = i
             elif not pixel_es_negro and en_segmento:
                 en_segmento = False
-                y_final = y - 1
-                segmentos_en_columna.append((y_inicio, y_final))
+                final = i - 1
+                if (final - inicio) >= min_largo:
+                    segmentos_en_linea.append((inicio, final))
         if en_segmento:
-            segmentos_en_columna.append((y_inicio, len(columna_actual) - 1))
-            
-        segmentos_por_columna[x] = segmentos_en_columna
+            final = len(linea_actual) - 1
+            if (final - inicio) >= min_largo:
+                segmentos_en_linea.append((inicio, final))
         
-    return segmentos_por_columna
-
-def encontrar_segmentos_horizontales(mascara_umbral, lineas_y):
-    """
-    Itera sobre cada coordenada Y de una línea y encuentra los
-    segmentos de línea (x_inicio, x_final) en esa fila.
-    """
-    segmentos_por_fila = {}
-    for y in lineas_y:
-        fila_actual = mascara_umbral[y, :]
-        segmentos_en_fila = []
-        en_segmento = False
-        x_inicio = 0
-
-        for x, pixel_es_negro in enumerate(fila_actual):
-            if pixel_es_negro and not en_segmento:
-                en_segmento = True
-                x_inicio = x
-            elif not pixel_es_negro and en_segmento:
-                en_segmento = False
-                x_final = x - 1
-                segmentos_en_fila.append((x_inicio, x_final))
-                
-        if en_segmento:
-            segmentos_en_fila.append((x_inicio, len(fila_actual) - 1))
+        # Opcional: solo añadimos la coordenada al diccionario si encontramos segmentos válidos
+        if segmentos_en_linea:
+            segmentos_dict[coord] = segmentos_en_linea
             
-        segmentos_por_fila[y] = segmentos_en_fila
-        
-    return segmentos_por_fila
+    return segmentos_dict
 
 if __name__ == '__main__':
     img, mascara, vert, hor = detectar_lineas('formulario_vacio.png', 180, 170, 200)
     img_dummy = cv2.cvtColor(img, cv2.COLOR_GRAY2BGR)
-    segmentos_ver = encontrar_segmentos_verticales(mascara,vert)
-    segmentos_hor = encontrar_segmentos_horizontales(mascara,hor)
+    segmentos_ver = encontrar_segmentos(mascara,vert, 'vertical')
+    segmentos_hor = encontrar_segmentos(mascara,hor, 'horizontal')
     color_horizontal = (255, 0, 0)  # Azul
-    grosor_linea = 2
+    grosor_linea = 1
     for y, segs in segmentos_hor.items():
         for x_start, x_end in segs:
             cv2.line(img_dummy, (x_start, y), (x_end, y), color_horizontal, grosor_linea)
