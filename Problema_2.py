8faef37221d76585ec837b389b0bda8f858ac1f0
martinfl95 <91093784+martinfl95@users.noreply.github.com>
2025-10-16 23:09:32 -0300

    Extracción de celdas con margenes recortados y visualización
    Signed-off-by: martinfl95 <91093784+martinfl95@users.noreply.github.com>

--- Problema_2.py
+++ Problema_2.py
@@ -2,18 +2,19 @@ import numpy as np
 import matplotlib.pyplot as plt
 import cv2
 
+
 def detectar_lineas(nombre_archivo, umbral_mascara, umbral_vert, umbral_hor):
     img = cv2.imread(filename=nombre_archivo, flags=cv2.IMREAD_GRAYSCALE)
-    mascara_umbral = img < umbral_mascara #type: ignore
-    #Sumamos los True en las columnas para generar un vector que nos diga
-    #en qué coordenada X hay más concentracion de pixeles "blancos" (con valor 1)
+    mascara_umbral = img < umbral_mascara  # type: ignore
+    # Sumamos los True en las columnas para generar un vector que nos diga
+    # en qué coordenada X hay más concentracion de pixeles "blancos" (con valor 1)
     proyeccion_vertical = np.sum(mascara_umbral, axis=0)
     proyeccion_horizontal = np.sum(mascara_umbral, axis=1)
     coord_x_lineas = list(np.where(proyeccion_vertical > umbral_vert)[0])
     coord_y_lineas = list(np.where(proyeccion_horizontal > umbral_hor)[0])
     x_unicas = coord_x_lineas[1::2]
     y_unicas = coord_y_lineas[1::2]
-    #return lineas verticales, lineas horizontales
+    # return lineas verticales, lineas horizontales
     return img, mascara_umbral, x_unicas, y_unicas
 
 
@@ -23,7 +24,7 @@ def encontrar_segmentos(mascara_umbral, coordenadas, eje='horizontal', min_largo
     filtrando aquellos que son más cortos que min_largo.
     """
     segmentos_dict = {}
-    
+
     for coord in coordenadas:
         if eje == 'horizontal':
             linea_actual = mascara_umbral[coord, :]
@@ -46,12 +47,13 @@ def encontrar_segmentos(mascara_umbral, coordenadas, eje='horizontal', min_largo
             final = len(linea_actual) - 1
             if (final - inicio) >= min_largo:
                 segmentos_en_linea.append((inicio, final))
-        
+
         # Opcional: solo añadimos la coordenada al diccionario si encontramos segmentos válidos
         if segmentos_en_linea:
             segmentos_dict[coord] = segmentos_en_linea
-            
+
     return segmentos_dict
+
 
 def encontrar_intersecciones(segmentos_hor, segmentos_ver):
     """
@@ -64,105 +66,144 @@ def encontrar_intersecciones(segmentos_hor, segmentos_ver):
             # Ahora, iteramos sobre cada línea vertical y sus segmentos
             for x, segs_verticales in segmentos_ver.items():
                 for y_start, y_end in segs_verticales:
-                    
+
                     # Esta es la condición clave:
                     if (x_start <= x <= x_end) and (y_start <= y <= y_end):
                         vertices.append((x, y))
-                        
+
     return vertices
 
+
+def encontrar_celdas(img, segmentos_hor, segmentos_ver, margen=2):
+    filas = []
+    seg_hor = sorted(list(segmentos_hor.keys()))
+    for i in range(len(seg_hor) - 1):
+        fila_recortada = img[seg_hor[i] + margen: seg_hor[i+1] - margen, :]
+        filas.append(fila_recortada)
+
+    seg_ver = sorted(list(segmentos_ver.keys()))
+    celdas = {}
+
+    celdas['titulo'] = filas[0][:, seg_ver[0]+margen: seg_ver[3]-margen]
+
+    celdas['nombre'] = filas[1][:, seg_ver[0]+margen: seg_ver[1]-margen]
+    celdas['nombre_valor'] = filas[1][:, seg_ver[1]+margen: seg_ver[3]-margen]
+
+    celdas['edad'] = filas[2][:, seg_ver[0]+margen: seg_ver[1]-margen]
+    celdas['edad_valor'] = filas[2][:, seg_ver[1]+margen: seg_ver[3]-margen]
+
+    celdas['mail'] = filas[3][:, seg_ver[0]+margen: seg_ver[1]-margen]
+    celdas['mail_valor'] = filas[3][:, seg_ver[1]+margen: seg_ver[3]-margen]
+
+    celdas['legajo'] = filas[4][:, seg_ver[0]+margen: seg_ver[1]-margen]
+    celdas['legajo_valor'] = filas[4][:, seg_ver[1]+margen: seg_ver[3]-margen]
+
+    celdas['pregunta1'] = filas[6][:, seg_ver[0]+margen: seg_ver[1]-margen]
+    celdas['pregunta1_si'] = filas[6][:, seg_ver[1]+margen: seg_ver[2]-margen]
+    celdas['pregunta1_no'] = filas[6][:, seg_ver[2]+margen: seg_ver[3]-margen]
+
+    celdas['pregunta2'] = filas[7][:, seg_ver[0]+margen: seg_ver[1]-margen]
+    celdas['pregunta2_si'] = filas[7][:, seg_ver[1]+margen: seg_ver[2]-margen]
+    celdas['pregunta2_no'] = filas[7][:, seg_ver[2]+margen: seg_ver[3]-margen]
+
+    celdas['pregunta3'] = filas[8][:, seg_ver[0]+margen: seg_ver[1]-margen]
+    celdas['pregunta3_si'] = filas[8][:, seg_ver[1]+margen: seg_ver[2]-margen]
+    celdas['pregunta3_no'] = filas[8][:, seg_ver[2]+margen: seg_ver[3]-margen]
+
+    celdas['comentario'] = filas[9][:, seg_ver[0]+margen: seg_ver[1]-margen]
+    celdas['comentario_valor'] = filas[9][:,seg_ver[1]+margen: seg_ver[3]-margen]
+
+    return celdas
+
+def mostrar_celda(ax, titulo, imagen):
+    ax.imshow(imagen, cmap='gray', vmin=0, vmax=255)
+    ax.set_title(titulo)
+    ax.axis('on')
+    ax.set_xticks([])
+    ax.set_yticks([])
+    
+def mostrar_formulario_desarmado(celdas):
+    """
+    Muestra todas las celdas extraídas en una grilla que simula el formulario.
+    """
+    fig, axs = plt.subplots(10, 3, figsize=(10, 15))
+
+    for ax_row in axs:
+        for ax in ax_row:
+            ax.axis('off')
+
+    mostrar_celda(axs[1, 0], 'nombre', celdas['nombre'])
+    ax_nombre_valor = plt.subplot2grid((10, 3), (1, 1), colspan=2)
+    mostrar_celda(ax_nombre_valor, 'nombre_valor', celdas['nombre_valor'])
+    
+    mostrar_celda(axs[2, 0], 'edad', celdas['edad'])
+    ax_edad_valor = plt.subplot2grid((10, 3), (2, 1), colspan=2)
+    mostrar_celda(ax_edad_valor, 'edad_valor', celdas['edad_valor'])
+
+    mostrar_celda(axs[3, 0], 'mail', celdas['mail'])
+    ax_mail_valor = plt.subplot2grid((10, 3), (3, 1), colspan=2)
+    mostrar_celda(ax_mail_valor, 'mail_valor', celdas['mail_valor'])
+    mostrar_celda(axs[4, 0], 'legajo', celdas['legajo'])
+    ax_legajo_valor = plt.subplot2grid((10, 3), (4, 1), colspan=2)
+    mostrar_celda(ax_legajo_valor, 'legajo_valor', celdas['legajo_valor'])
+    mostrar_celda(axs[6, 0], 'pregunta1', celdas['pregunta1'])
+    mostrar_celda(axs[6, 1], 'pregunta1_si', celdas['pregunta1_si'])
+    mostrar_celda(axs[6, 2], 'pregunta1_no', celdas['pregunta1_no'])
+
+    mostrar_celda(axs[7, 0], 'pregunta2', celdas['pregunta2'])
+    mostrar_celda(axs[7, 1], 'pregunta2_si', celdas['pregunta2_si'])
+    mostrar_celda(axs[7, 2], 'pregunta2_no', celdas['pregunta2_no'])
+    
+    mostrar_celda(axs[8, 0], 'pregunta3', celdas['pregunta3'])
+    mostrar_celda(axs[8, 1], 'pregunta3_si', celdas['pregunta3_si'])
+    mostrar_celda(axs[8, 2], 'pregunta3_no', celdas['pregunta3_no'])
+
+    mostrar_celda(axs[9, 0], 'comentario', celdas['comentario'])
+    ax_comentario_valor = plt.subplot2grid((10, 3), (9, 1), colspan=2)
+    mostrar_celda(ax_comentario_valor, 'comentario_valor', celdas['comentario_valor'])
+
+    plt.tight_layout()
+    plt.subplots_adjust(top=0.95, hspace=0.9)
+    fig.suptitle('Formulario Desarmado por Celdas', fontsize=16)
+    plt.show()
+
 if __name__ == '__main__':
-    figura = True
-    segmentos = True
-    vertices = True
-    img, mascara, vert, hor = detectar_lineas('formulario_01.png', 180, 170, 200)
+    figura_flag = False
+    segmentos_flag = False
+    vertices_flag = False
+    celdas_flag = True
+    
+    img, mascara, vert, hor = detectar_lineas(
+        'formulario_01.png', 180, 170, 200)
     img_dummy = cv2.cvtColor(img, cv2.COLOR_GRAY2BGR)
-    segmentos_ver = encontrar_segmentos(mascara,vert, 'vertical',30)
-    segmentos_hor = encontrar_segmentos(mascara,hor, 'horizontal',30)
+    segmentos_ver = encontrar_segmentos(mascara, vert, 'vertical', 30)
+    segmentos_hor = encontrar_segmentos(mascara, hor, 'horizontal', 30)
     vertices = encontrar_intersecciones(segmentos_hor, segmentos_ver)
+    celdas = encontrar_celdas(img, segmentos_hor, segmentos_ver, margen=2)
     color_horizontal = (255, 0, 0)  # Azul
     grosor_linea = 1
-    # vertices_ordenados = sorted(vertices, key=lambda p: (int(p[0]), int(p[1])))
-    # print(vertices_ordenados)
-    # # Paso 1: limpiar y ordenar
-    # xs = sorted(list(set(int(x) for x, y in vertices)))  # columnas
-    # ys = sorted(list(set(int(y) for x, y in vertices)))  # filas
-
-    # print(f"X únicos ({len(xs)}): {xs}")
-    # print(f"Y únicos ({len(ys)}): {ys}")
     
-    # M = np.zeros((len(ys), len(xs)), dtype=bool)
-    
-    # for (x, y) in vertices:
-    #     # buscamos el índice correspondiente en xs e ys
-    #     j = xs.index(int(x))  # columna
-    #     i = ys.index(int(y))  # fila
-    #     M[i, j] = True
-    # print("-----------------------------")
-    # print(M)
-    # cuadrados = []
-    # for i in range(len(ys)-1):
-    #     for j in range(len(xs)-1):
-    #         if M[i,j] and M[i,j+1] and M[i+1,j] and M[i+1,j+1]:
-    #             cuadrados.append(((xs[j], ys[i]), (xs[j+1], ys[i+1])))
-
-    # print("Total cuadrados detectados:", cuadrados)
-    
-    # for i in cuadrados:
-    #     cv2.rectangle(img_dummy, i[0], i[1], (0,255,255), 2)
-    # print("-----------------------")
-    # print(segmentos_hor)
-    
-    #Segmentamos recortes de imagen por filas
-    filas = []
-    seg_hor = list(segmentos_hor.keys())
-    for i in range(len(segmentos_hor.keys())-1):
-        filas.append(img[seg_hor[i]:seg_hor[i+1],:])
-    celdas = {}
-    celdas['titulo'] = filas[0]
-    celdas['nombre'] = filas[1][:,21:326]
-    celdas['nombreValor'] = filas[1][:,326:933]
-    celdas['edad'] = filas[2][:,21:326]
-    celdas['edadValor'] = filas[2][:,326:933]
-    celdas['mail'] = filas[3][:,21:326]
-    celdas['mailValor'] = filas[3][:,326:933]
-    celdas['legajo'] = filas[4][:,21:326]
-    celdas['legajoValor'] = filas[4][:,326:933]    
-    celdas['pregunta1'] = filas[6][:,21:326]
-    celdas['pregunta1Si'] = filas[6][2:,326+1:630-2] #filtracion de pixeles   
-    celdas['pregunta1No'] = filas[6][:,630:933]  
-    celdas['pregunta2'] = filas[7][:,21:326]
-    celdas['pregunta2Si'] = filas[7][:,326:630]   
-    celdas['pregunta2No'] = filas[7][:,630:933] 
-    celdas['pregunta3'] = filas[8][:,21:326]
-    celdas['pregunta3Si'] = filas[8][:,326:630]   
-    celdas['pregunta3No'] = filas[8][:,630:933]  
-    celdas['comentario'] = filas[9][:,21:326]
-    celdas['comentarioValor'] = filas[9][:,326:933]
-    
-    
-    print('filas---------')
-    print(segmentos_ver)
-    plt.imshow(cv2.cvtColor(celdas['pregunta1Si'] , cv2.COLOR_BGR2RGB))
-    plt.show() 
-    if(segmentos):
+    if (segmentos_flag):
         for y, segs in segmentos_hor.items():
             for x_start, x_end in segs:
-                cv2.line(img_dummy, (x_start, y), (x_end, y), color_horizontal, grosor_linea)
+                cv2.line(img_dummy, (x_start, y), (x_end, y),
+                         color_horizontal, grosor_linea)
 
         color_vertical = (0, 0, 255)  # Rojo
         for x, segs in segmentos_ver.items():
             for y_start, y_end in segs:
-                cv2.line(img_dummy, (x, y_start), (x, y_end), color_vertical, grosor_linea)
-    if(vertices):    
+                cv2.line(img_dummy, (x, y_start), (x, y_end),
+                         color_vertical, grosor_linea)
+    if (vertices_flag):
         for x, y in vertices:
-            # Dibujamos un pequeño círculo verde en cada vértice
-            cv2.circle(img_dummy, (x, y), radius=3, color=(0, 255, 0), thickness=-1)
-            
-    if(figura):
+            cv2.circle(img_dummy, (x, y), radius=3,
+                       color=(0, 255, 0), thickness=-1)
+
+    if (figura_flag):
         plt.figure(figsize=(12, 12))
-        # Convertimos de BGR (OpenCV) a RGB (Matplotlib) para mostrar colores correctos
         plt.imshow(cv2.cvtColor(img_dummy, cv2.COLOR_BGR2RGB))
-        plt.title('Todos los Segmentos Detectados sobre la Imagen Original')
         plt.axis('off')
         plt.show()
+    
+    if(celdas_flag):
+        mostrar_formulario_desarmado(celdas)
